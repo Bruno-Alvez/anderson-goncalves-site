7d7b20cf95b92cc01b779c3575ec5fc8569404b6
Bruno-Alvez <bruno.bsantos75@gmail.com>
2025-07-06 17:36:04 -0300

    chore: initial frontend setup with React, TypeScript, and Styled Components

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import { GlobalStyles } from './styles/GlobalStyles'
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <div>Hello, Anderson Gonçalves 👊</div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/styles/GlobalStyles.ts
@@ -0,0 +1,22 @@
+// src/styles/GlobalStyles.ts
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+    background-color: #f9f9f9;
+    color: #111;
+    line-height: 1.6;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`
